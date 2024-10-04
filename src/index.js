65a00ab11315a814cdfef172854ee617c39e165c
dharmaraj1998 <4344dharmaraj@gmail.com>
2024-10-04 19:16:51 +0530

    jsx

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App(){
+  const date = new Date();
+  const name = "samantha";
+  const age = 25;
   return(
     <div>
-      <h1>hi there</h1>
+      <h1>{date.toLocaleDateString()} </h1>
+      <p>{name} {age}</p>
     </div>
   )
 }
